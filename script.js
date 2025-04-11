2c4b9fe0df0637bea4700bd4d6fcc324ef4825f7
r00t0ver <tanvir.22464751@gmail.com>
2025-04-11 23:46:49 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function getKeyArray(key) {
+    const keyArr = [];
+    for (let i = 0; i < key.length; i++) {
+      keyArr.push(key.charCodeAt(i) % 48);
+    }
+    return keyArr;
+  }
+
+  function encrypt() {
+    const str = document.getElementById('message').value;
+    const kk = document.getElementById('key').value;
+    const key = getKeyArray(kk);
+    let msg = "", j = 0;
+    for (let i = 0; i < str.length; i++) {
+      if (j === key.length) j = 0;
+      msg += String.fromCharCode(str.charCodeAt(i) + key[j]);
+      j++;
+    }
+    document.getElementById('output').innerHTML = "<span style='color:red'>Encrypted: " + msg + "</span>";
+  }
+
+  function decrypt() {
+    const str = document.getElementById('message').value;
+    const kk = document.getElementById('key').value;
+    const key = getKeyArray(kk);
+    let msg = "", j = 0;
+    for (let i = 0; i < str.length; i++) {
+      if (j === key.length) j = 0;
+      msg += String.fromCharCode(str.charCodeAt(i) - key[j]);
+      j++;
+    }
+    document.getElementById('output').innerHTML = "<span style='color:green'>Decrypted: " + msg + "</span>";
+  }
